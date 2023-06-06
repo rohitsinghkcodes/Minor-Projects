3ea7acb08015ee9ae794decdf2e67fab4f6a434f
Rohit <mailsrohitk@gmail.com.com>
2023-06-06 08:37:44 +0530

    some more projects added

---
+++ Flutter Web App/firstweb/android/app/src/main/kotlin/com/example/firstweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firstweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ translator letter/main.py
@@ -0,0 +1,66 @@
+# incomplete project
+
+
+
+import os
+
+from PyPDF2 import PdfFileReader
+from googletrans import Translator
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import Paragraph
+from reportlab.platypus import SimpleDocTemplate
+
+from translate_pdfs.fonts import *
+
+"""
+This script uses Google Translate library to translate the PDF
+"""
+
+"""
+Constants
+"""
+URL_COM = 'translate.googleapis.com'
+LANG = "lv"
+
+"""
+FUNCTIONS
+"""
+
+
+def get_translated_page_content(reader, lang):
+    """
+    Reads page content from the reader, translates it,
+    cleans it and returns page content as a list of strings.
+    Each entry in list represents a page
+    """
+    num_pages = reader.numPages
+    page_contents = []
+    translator = Translator(service_urls=[URL_COM])
+    for p in range(num_pages):
+        page = reader.getPage(p)
+        text = page.extractText()
+        translation = translator.translate(text, dest=lang)
+        result_text = translation.text.replace("\n", " ").replace("W", "")
+        page_contents.append(result_text)
+    return page_contents
+
+
+def translate_pdf(path, lang):
+    file = open(path, 'rb')
+    reader = PdfFileReader(file)
+    page_contents = get_translated_page_content(reader, lang)
+
+    page_text = []
+    name = f'{LANG}_{path}'
+    pdf = SimpleDocTemplate(name, pagesize=letter)
+
+    for text in page_contents:
+        page_text.append(
+            Paragraph(text, encoding='utf-8', style=regular))
+
+    pdf.build(page_text)
+
+
+if __name__ == '__main__':
+    file_name = "example.pdf"
+    translate_pdf(file_name, LANG)
